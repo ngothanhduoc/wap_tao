327477fed1d4fa1d803fd628cfac5826360745c2
ngothanhduoc <ngothanhduoc1991@gmail.com>
2015-07-29 08:53:27 +0700

    a

---
+++ application/views/layouts/wap/default.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<meta name="description" content="Admin Panel" />
+<meta name="author" content="mecorp.vn" />
+<title>Me | AdminCP</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Me | AdminCP</title>
+
+<link rel="stylesheet" href="/admin/assets/css/style.default.css" type="text/css" />
+
+<script type="text/javascript" src="/admin/assets/js/plugins/jquery-1.8.3.min.js"></script>
+
+
+
+<script type="text/javascript">
+	
+</script>
+
+
+</head>
+
+<body class="withvernav">
+
+<div class="bodywrapper">
+    <div class="topheader">
+	<?php echo $breakcrumb; ?>
+    </div><!--topheader-->
+    
+    
+    <div class="header">      
+    </div><!--header-->
+    
+    <div class="vernav2 iconmenu">
+	<?php echo $leftmenu;?>
+    	
+    </div><!--leftmenu-->
+        
+    <div class="centercontent">
+	<?php echo $content; ?>
+    </div><!-- centercontent -->
+    
+    
+</div><!--bodywrapper-->
+
+</body>
+</html>
